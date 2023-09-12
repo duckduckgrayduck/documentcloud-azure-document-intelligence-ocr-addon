66509bc431b9ba3cd4e69518239b238b3bac6be2
s <102841251+duckduckgrayduck@users.noreply.github.com>
2023-09-12 11:36:52 -0500

    First draft at Azure OCR run

--- README.md
+++ README.md
@@ -3,14 +3,6 @@
 
 [Please see the Add-On documentation](https://github.com/MuckRock/documentcloud-hello-world-addon/wiki/)
 
-# DocumentCloud Hello World Add-On
+# DocumentCloud Azure Document Intelligence OCR Add-On
 
-This repository contains an example Add-On for DocumentCloud.  It is designed
-to be copied and modified to allow one to easily write Add-Ons to bring custom
-functionality to DocumentCloud.  Please use the green "Use this Template"
-button to create a new repository instead of forking.  This will be easier as
-the new Add-On will diverge significantly from this code base.
-
-After copying the template, remember to update this README file to describe
-your new Add-On!  It is a good idea to leave the link to the Add-On
-documentation at the top of the file though.
+This DocumentCloud Add-On uses [Azure's Document Intelligence API](https://learn.microsoft.com/en-us/azure/ai-services/document-intelligence/how-to-guides/use-sdk-rest-api?view=doc-intel-3.0.0&preserve-view=true%3Fpivots%3Dprogramming-language-python&tabs=linux&pivots=programming-language-python) to perform OCR on a document. 
--- main.py
+++ main.py
@@ -1,38 +1,36 @@
 """
-This is a hello world add-on for DocumentCloud.
-
-It demonstrates how to write a add-on which can be activated from the
-DocumentCloud add-on system and run using Github Actions.  It receives data
-from DocumentCloud via the request dispatch and writes data back to
-DocumentCloud using the standard API
+This Add-On uses Azure's Document Intelligence API
+to perform OCR on documents within DocumentCloud
 """
-
+import os
+from azure.ai.formrecognizer import DocumentAnalysisClient
+from azure.core.credentials import AzureKeyCredential
 from documentcloud.addon import AddOn
 
-
-class HelloWorld(AddOn):
-    """An example Add-On for DocumentCloud."""
+class DocumentIntelligence(AddOn):
+    """Class for Document Intelligence Add-On"""
 
     def main(self):
         """The main add-on functionality goes here."""
-        # fetch your add-on specific data
-        name = self.data.get("name", "world")
-
-        self.set_message("Hello World start!")
-
-        # add a hello note to the first page of each selected document
+        key = os.environ.get('KEY')
+        endpoint = os.environ.get('TOKEN')
+        document_analysis_client = DocumentAnalysisClient(
+            endpoint=endpoint, credential=AzureKeyCredential(key)
+        )
         for document in self.get_documents():
-            # get_documents will iterate through all documents efficiently,
-            # either selected or by query, dependeing on which is passed in
-            document.annotations.create(f"Hello {name}!", 0)
-
-        with open("hello.txt", "w+") as file_:
-            file_.write("Hello world!")
-            self.upload_file(file_)
-
-        self.set_message("Hello World end!")
-        self.send_mail("Hello World!", "We finished!")
-
+            poller = document_analysis_client.begin_analyze_document_from_url(
+                "prebuilt-read", formUrl
+            )
+            result = poller.result()
+            pages = []
+            for i,page in enumerate(result.pages):
+                dc_page = {
+                    "page_number": i,
+                    "text": '\n'.join([line.content for line in page.lines]),
+                    "ocr": "azuredi",
+                    "positions": [],
+                }
+                pages.append(dc_page)
 
 if __name__ == "__main__":
-    HelloWorld().main()
+    DocumentIntelligence().main()
